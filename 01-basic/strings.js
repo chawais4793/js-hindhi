d0c9657d12f8c4c4b344459a247f47b3689c41da
chawais4793 <151781883+chawais4793@users.noreply.github.com>
2024-10-22 09:49:10 +0000

    strings .js

---
+++ 01-basic/strings.js
@@ -0,0 +1,43 @@
+const name="Awais"
+const channel="youtube"
+
+//kisy bi ccheez ko concatinate krny ka lia modern (``)is ka use hota ha
+
+
+console.log(`my name is ${name} and my channel name is ${channel}`);
+
+const gamename=new String('Awaisch')
+console.log(gamename)
+
+//for use lemgth of variabel
+console.log(gamename.length);
+
+// for use knsy num pa variabel ha//
+console.log(gamename.charAt(5));
+//for use kis num pa koi variabel haa
+console.log(gamename.indexOf('i'));
+//for uppercase
+console.log(gamename.toUpperCase());
+//for lowercase
+console.log(gamename.toLowerCase());
+
+
+
+
+const newstring=gamename.substring(1,7);
+console.log(newstring);
+
+const pc ="asus".substring(0,3)
+console.log(pc);
+
+
+const mouse="jerry" .slice(2,4)
+console.log(mouse);
+  
+const keybord= "    qwetry " 
+console.log(keybord );
+
+
+const url= "https/wwww.awais.com"
+console.log(url.replace ('com','pk'));
+
