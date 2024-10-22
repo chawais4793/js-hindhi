c8ca312c45332a8bbc197a9c411a62ec013f8396
chawais4793 <151781883+chawais4793@users.noreply.github.com>
2024-10-22 20:16:25 +0000

    nummaths.js

---
+++ 01-basic/nummath.js
@@ -0,0 +1,33 @@
+const score= "200"
+console.log(score)
+
+
+const balance= new Number(100)
+console.log(balance);
+console.log(typeof balance);
+
+
+console.log(balance .toString())
+console.log(typeof balance)
+
+//tofixesd use for after how much zero you want of num e.g 100.00
+console.log(balance.toFixed(2));
+
+
+const other = 234.2365
+console.log(other .toPrecision(2))
+
+
+//+++++++++++++++++++++++number++++++++++++++++++++++++++++++
+console.log(Math.max(2,3,4,5));
+console.log(Math.random());
+console.log(Math.floor(5.4));
+console.log(Math.ceil(5.4));
+
+console.log(Math.random()*5+1);
+
+
+const max= 10
+const min= 5
+  
+console.log(Math.floor((Math.random()*(max-min+1)))+min)
