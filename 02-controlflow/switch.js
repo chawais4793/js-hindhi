aa81f4bb3676182facfa99e3df259c30bd46ffbc
chawais4793 <151781883+chawais4793@users.noreply.github.com>
2024-11-05 10:10:50 +0000

    switch.js

---
+++ 02-controlflow/switch.js
@@ -0,0 +1,20 @@
+const month=3
+switch (month) {
+    case 1:
+        console.log("januwary");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    
+    default:
+        console.log("may");
+        
+        break;
+}
