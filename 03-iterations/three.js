8634d8973c270b72291522ffb1d7aad598dffd70
chawais4793 <151781883+chawais4793@users.noreply.github.com>
2024-11-07 11:04:23 +0000

    threee.js

---
+++ 03-iterations/three.js
@@ -0,0 +1,36 @@
+const array=[1,2,3,4,5]
+for (const arr of array) {
+   // console.log(arr);
+    
+}
+
+const name=("hello world")
+for (const greet of name) {
+  //  console.log(`each value of ${greet}`);
+    
+}
+
+//maps
+const map  =new Map()
+map.set('PK',"Pakistan")
+map.set('IN', "india")
+map.set('UAE', "Dubai")
+
+// console.log(map);
+
+
+for (const  [key, value] of map) {
+   // console.log(key,value);
+    
+    
+}
+
+const Object={
+    game1: "Need for speed",
+    game2: "gta vi city",
+
+}
+for (const [key, value] of Object) {
+    console.log(Object);
+    
+}
