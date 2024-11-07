546a99ec082d41eb16ddf1445ebc937f9e82cf6e
chawais4793 <151781883+chawais4793@users.noreply.github.com>
2024-11-07 11:06:40 +0000

    four.js

---
+++ 03-iterations/four.js
@@ -0,0 +1,12 @@
+//for in loop for use object for iterate 
+
+const Object={
+    game1: "Need for speed",
+    game2: "gta vi city",
+
+}
+for (const key in Object) {
+    console.log(`${key} for use gamming${Object[key]}`) ;
+    
+
+}
